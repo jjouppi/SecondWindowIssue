7316243b6ad4e13b9951b0141e02f7b80886f660
joshu <joshua.jouppi@gmail.com>
2020-06-26 11:21:42 -0600

    Issue demonstrated

---
+++ SecondWndowIssueTest_v01/Views/BlankPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SecondWndowIssueTest_v01.Views
+{
+    public sealed partial class BlankPage : Page, INotifyPropertyChanged
+    {
+        public BlankPage()
+        {
+            InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            //Sets Window Size at open
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size { Width = 350, Height = 127 });
+            ApplicationView.PreferredLaunchViewSize = new Size(350, 127);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
